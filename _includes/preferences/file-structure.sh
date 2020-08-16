75bdf9fc46a9769a30cbf8aab20fd6e233d1dc65
Newton Koumantzelis <5769156+iamnewton@users.noreply.github.com>
2020-08-16 11:41:17 -0700

    chore: set up some new stuff, not tested yet

---
+++ _includes/preferences/file-structure.sh
@@ -0,0 +1,24 @@
+// Bad
+
+```
+src/
+├── controllers/
+|   ├── product.js
+|   └── user.js
+├── models/
+|   ├── product.js
+|   └── user.js
+```
+
+// Good
+```
+src/
+├── product/
+|   ├── index.js
+|   ├── product.js
+|   └── product.test.js
+├── user/
+|   ├── index.js
+|   ├── user.js
+|   └── user.test.js
+```
